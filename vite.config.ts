b760278e6b7ed3d64e419bf03acb56362268a04d
Somesetty manideep <Manideep723@gmail.com>
2026-03-06 13:19:01 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/",
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-
   build: {
-    target: "es2018"   // 🔥 important fix
+    target: "es2018"
   },
-
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+});
